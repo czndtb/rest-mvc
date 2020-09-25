f0f127a149170cfa11b8eae1d91c7cd08ef9c5c7
yami <1315472623lkj@gmail.com>
2020-09-25 14:02:05 +0800

    test prop

--- src/main/java/com/ccc/restmvc/controller/TestController.java
+++ src/main/java/com/ccc/restmvc/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.ccc.restmvc.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 
 import javax.validation.Valid;
 
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ccc.restmvc.entity.PostBody;
@@ -40,7 +44,19 @@ public class TestController {
 
 	@GetMapping("get")
 	public String getMethod(@RequestParam(required = true) Integer id) {
-		return "ok";
+		
+		InputStream in = getClass().getClassLoader().getResourceAsStream("test.properties");
+		
+		Properties prop = new Properties();
+		
+		try {
+			prop.load(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return JSONObject.toJSONString(prop);
 	}
 
 	@GetMapping("get/{id}")
